f2a51fbc8117727a1d351a71739b9360489e485c
Jeroen Zwart <jeroen.zwart@live.com>
2021-04-26 17:35:17 +0200

    created reusable component for form fields

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, useField, Form } from 'formik';
 import { NotificationManager } from 'react-notifications';
 import axios from 'axios';
 import * as Yup from 'yup';
@@ -52,62 +52,95 @@ const RegistrationForm = () => {
         }
     }
 
+    const TextField = ({ ...props }) => {
+        const [field, meta] = useField(props);
+        return (
+            <>
+                <input className="form-control mt-3" {...field} {...props} />
+                {meta.touched && meta.error ? (
+                <div className="font-weight-bold text-danger">{meta.error}</div>
+                ) : null}
+            </>
+            );
+        };
+
     return (
         <Formik 
-        initialValues={{
-            initials: '',
-            insertion: '',
-            lastname: '',
-            zip: '',
-            streetname: '',
-            city: '',
-            number: '',
-            email: ''
-        }}
-        validationSchema={Yup.object({
-            initials: Yup.string()
-                .required('Verplicht veld'),
-            lastname: Yup.string()
-                .required('Verplicht veld'),
-            zip: Yup.string()
-                .required('Verplicht veld')
-                .matches(/^\d{4} ?[a-z]{2}$/i, {message: 'Ongeldige postcode'}),
-            email: Yup.string()
-                .required('Verplicht veld')
-                .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, {message: 'Ongeldig e-mailadres'}),
-        })}
-        onSubmit={values => {
-            postDetails(values, street, city)}
-        }
+            initialValues={{
+                initials: '',
+                insertion: '',
+                lastname: '',
+                zip: '',
+                streetname: '',
+                city: '',
+                number: '',
+                email: ''
+            }}
+            validationSchema={Yup.object({
+                initials: Yup.string()
+                    .required('Verplicht veld'),
+                lastname: Yup.string()
+                    .required('Verplicht veld'),
+                zip: Yup.string()
+                    .required('Verplicht veld')
+                    .matches(/^\d{4} ?[a-z]{2}$/i, {message: 'Ongeldige postcode'}),
+                email: Yup.string()
+                    .required('Verplicht veld')
+                    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, {message: 'Ongeldig e-mailadres'}),
+            })}
+            onSubmit={values => {
+                postDetails(values, street, city)}
+            }
         >
-
             {(formik) => (
                 <div className="registration-form d-flex flex-column mt-5">
                 <h1>Schrijf je in</h1>
                 <Form>
-                    <Field name="initials" type="text" placeholder="Initialen" className="form-control mt-3"/>
-                    <ErrorMessage name="initials" className="font-weight-bold text-danger"/>
-
-                    <Field name="insertion" type="text" placeholder="Tussenvoegsel" className="form-control mt-3"/>
-
-                    <Field name="lastname" type="text" placeholder="Achternaam" className="form-control mt-3"/>
-                    <ErrorMessage name="lastname" className="font-weight-bold text-danger"/>
-
-                    <Field name="zip" type="text" placeholder="Postcode" className="form-control mt-3" />
-                    <ErrorMessage name="zip" className="font-weight-bold text-danger"/>
-
-                    <Field name="number" type="number" placeholder="Huisnummer" className="form-control mt-3" onBlur={() => { getAddress(formik.values.zip, formik.values.number) }}/>
-                    <ErrorMessage name="number" className="font-weight-bold text-danger"/>
-
-                    <Field name="streetname" type="text" placeholder="Straatnaam" className="form-control mt-3" value={formik.values.streetname || street}/>
-                    <ErrorMessage name="zip" className="font-weight-bold text-danger"/>
-
-                    <Field name="city" type="text" placeholder="Stad" className="form-control mt-3" value={formik.values.city || city}/>
-                    <ErrorMessage name="city" className="font-weight-bold text-danger"/>
-
-                    <Field name="email" type="text" placeholder="Email" className="form-control mt-3" />
-                    <ErrorMessage name="email" className="font-weight-bold text-danger"/>
-
+                    <TextField
+                        name="initials"
+                        type="text"
+                        placeholder="Initialen"
+                    />
+                    {/* <Field name="initials" type="text" placeholder="Initialen" className="form-control mt-3"/>
+                    <ErrorMessage name="initials" className="font-weight-bold text-danger"/> */}
+                    <TextField
+                        name="insertion"
+                        type="text"
+                        placeholder="Tussenvoegsel"
+                    />
+                    <TextField
+                        name="lastname"
+                        type="text"
+                        placeholder="Achternaam"
+                    />
+                    <TextField
+                        name="zip"
+                        type="text"
+                        placeholder="Postcode"
+                    />
+                    <TextField
+                        name="number"
+                        type="number"
+                        placeholder="Huisnummer"
+                        onBlur={() => { getAddress(formik.values.zip, formik.values.number) }}
+                    />
+                    <TextField
+                        name="streetname"
+                        type="text"
+                        placeholder="Stad"
+                        value={formik.values.streetname || street}
+                    />
+                    <TextField
+                        name="city"
+                        type="text"
+                        placeholder="Stad"
+                        value={formik.values.city || city}
+                    />
+                    <TextField
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                    />
                     { isLoading ? (
                     <button className="btn btn-primary mt-3" type="submit" disabled>
                     <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
